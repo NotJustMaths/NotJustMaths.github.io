45b2b6facaee67b2e59522179be5b12e7b612c1d
NotJustMaths <not.just.math@outlook.com>
2026-04-29 18:12:13 -0500

    Trying to fix the runnable functions

--- client.js
+++ client.js
@@ -1,15 +1,15 @@
 const supabase_client = window.supabase.createClient(
     'https://uzabwbqayjuhjdjgimct.supabase.co', 
     'sb_publishable_8En9l4Z_3-UPFZTAv2pE-A_HQPXEz3s'
-)
+);
 
 async function loadTables(table) {
     const { data, error } = await supabase_client
         .from(table)
-        .select()
+        .select();
 }
 
 function parseData() {
-    var parsedData = JSON.parse(data)
-    console.log(parsedData.data[0].id)
+    var parsedData = JSON.parse(data);
+    console.log(parsedData);
 }
