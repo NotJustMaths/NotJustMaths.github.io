6cd46b64c8083aad511e3c231b8970ea41ab7d75
NotJustMaths <not.just.math@outlook.com>
2026-04-29 17:54:04 -0500

    Differentiate the supabase client

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
-const supabase = window.supabase.createClient(
+const supabase_client = window.supabase.createClient(
     'https://uzabwbqayjuhjdjgimct.supabase.co', 
     'sb_publishable_8En9l4Z_3-UPFZTAv2pE-A_HQPXEz3s'
 )
 
 async function loadTables(table) {
-    const { data, error } = await supabase
+    const { data, error } = await supabase_client
         .from(table)
         .select()
 }
